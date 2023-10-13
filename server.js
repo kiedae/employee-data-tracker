d60321cbd12c19b844f7c443df9ca9d3e00fa1f0
Kiedae <ernestwilliams335@gmail.com>
2023-10-12 22:38:50 -0400

    added middleware, mysql connection, outline for prompt

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // replace with your MySQL username IF your default user isn't 'root'
+      user: 'root',
+      // Replace with your MySQL password
+      password: '1234',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
+
+inquirer.prompt([
+    {
+        type: input, 
+    }
+]);
+  
