9df8ebbd1039a2c03c4654b7d3c659cec6199347
Kiedae <ernestwilliams335@gmail.com>
2023-10-18 18:52:47 -0400

    created methods for accessing database info from inquirier prompt

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
-const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// express middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// mysql connection to login and get the company_db database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // replace with your MySQL username IF your default user isn't 'root'
-      user: 'root',
-      // Replace with your MySQL password
-      password: '1234',
-      database: 'company_db'
-    },
-    console.log(`Connected to the company_db database.`)
-  );
-
+const {viewDpt, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateRole} = require('./lib/methods');
   //PROMPT FOR OPTIONS
-inquirer.prompt({
+const prompting = () => { 
+  inquirer.prompt({
   type: 'list',
   name: 'choices',
   message: 'What would you like to do?',
   choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role', 'Finished']
 }) .then((data) => {
   switch (data['choices']) {
+    case 'View All Departments':
+        viewDpt();
+      break;
 
-  }
+    case 'View All Roles': 
+        viewRoles();
+      break;
+    
+    case 'View All Employees':
+        viewEmployees();
+      break;
 
+    case 'Add a Department':
+        addDepartment();
+      break;
+    
+    case 'Add a Role':
+        addRole();
+      break;
 
+    case 'Add an Employee':
+        addEmployee();
+      break;
+    
+    case 'Update Employee Role':
+        updateRole();
+      break;
+    
+    case 'Finished':
+      break;
+   }
+ });
+};
 
-
-
-
-
-});
-  
+prompting();
+module.exports = { prompting }; 
